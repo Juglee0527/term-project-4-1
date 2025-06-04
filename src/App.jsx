9e29b06f59c615af98603a244bcc5eff9040623b
Juglee <jason0101084@gmail.com>
2025-06-04 22:51:15 +0900

    feat: header, footer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import CompanyInfo from './components/CompanyInfo';
 import KakaoMap from './components/KakaoMap';
-import './App.css';
 
 function App() {
     return (
         <div className="container">
+            <Header />
             <CompanyInfo />
             <KakaoMap />
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>하이지노 기업 소개 페이지</h1>
+        </header>
+    );
+}
+
+export default Header;
